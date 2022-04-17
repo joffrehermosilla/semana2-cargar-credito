3cc27fcd594185b82f60b66654034577edea0ec1
Joffre Hermosilla Salas <jhermoss@emeal.nttdata.com>
2022-04-17 18:25:36 -0500

    primer commit

---
+++ microservicio-cargar-credito/src/main/java/nttdata/bootcamp/microservicios/cargar/creditos/MicroservicioCargarCreditoApplication.java
@@ -0,0 +1,13 @@
+package nttdata.bootcamp.microservicios.cargar.creditos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioCargarCreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioCargarCreditoApplication.class, args);
+	}
+
+}
