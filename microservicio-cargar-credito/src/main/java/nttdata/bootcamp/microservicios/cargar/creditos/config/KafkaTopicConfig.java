84ed5c0288a4ecbcc3dfa8f888fea7c310a61439
Joffre Hermosilla Salas <joffre.hermosilla@gmail.com>
2022-05-05 17:09:26 -0500

    kafka redis

---
+++ microservicio-cargar-credito/src/main/java/nttdata/bootcamp/microservicios/cargar/creditos/config/KafkaTopicConfig.java
@@ -0,0 +1,8 @@
+package nttdata.bootcamp.microservicios.cargar.creditos.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaTopicConfig {
+
+}
